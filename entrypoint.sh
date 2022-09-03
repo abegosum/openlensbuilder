c685d4df44fd8cef7de1464b9d43e9742d61d63c
Aaron M. Bond <abond@mpr.org>
2022-09-03 00:14:57 -0500

    Initial build working, have to export

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+LATEST_RELEASE_INFO_URL='https://api.github.com/repos/lensapp/lens/releases/latest'
+
+LATEST_TAG=`curl -sL ${LATEST_RELEASE_INFO_URL} | jq -r ".tag_name"`
+
+echo "Latest OpenLens release is ${LATEST_TAG}"
+
+mkdir /tmp/buildtemp
+
+cd /tmp/buildtemp
+
+git clone -b ${LATEST_TAG} https://github.com/lensapp/lens.git lens
+
+cd lens
+
+ELECTRON_BUILDER_EXTRA_ARGS='--linux AppImage' make build
