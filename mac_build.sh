97fe908e44655f81a4b1c2197eba886152bda503
Aaron M. Bond <abond@mpr.org>
2023-03-07 09:47:22 -0600

    Added mac builder

---
+++ mac_build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LATEST_RELEASE_INFO_URL='https://api.github.com/repos/lensapp/lens/releases/latest'
+
+LATEST_TAG=`curl -sL ${LATEST_RELEASE_INFO_URL} | jq -r ".tag_name"`
+
+echo "Latest OpenLens release is ${LATEST_TAG}"
+
+mkdir buildtemp
+
+cd buildtemp
+
+git clone -b ${LATEST_TAG} https://github.com/lensapp/lens.git lens 2>&1
+
+cd lens
+
+yarn install --check-files --frozen-lockfile --network-timeout=100000
+
+sed -i 's/electron-builder --publish onTag",/electron-builder --publish onTag --mac dmg",/' packages/open-lens/package.json
+
+yarn lerna run build:app
+
+cp packages/open-lens/dist/*.dmg ../../distout
